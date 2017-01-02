dd142bae233f6e80b9fe6bccf4a70b2c79d4499a
bemolxd <kalkobb99@gmail.com>
2017-01-02 21:16:49 +0100

    Add more OTA devices

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@ PRODUCT_AAPT_CONFIG := normal xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Recovery allowed devices
-TARGET_OTA_ASSERT_DEVICE := x2xtreme
+TARGET_OTA_ASSERT_DEVICE := x2xtreme,purexl,BLU_PURE_XL
 
 PRODUCT_PACKAGES += \
    libmtk_symbols \
