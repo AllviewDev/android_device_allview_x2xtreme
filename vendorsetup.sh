1b1edb8d943839404ba8555bc8fa5e0e652af04b
bemolxd <kalkobb99@gmail.com>
2017-01-02 21:18:04 +0100

    fix: It's CM!

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo full_x2xtreme-user
-add_lunch_combo full_x2xtreme-userdebug
-add_lunch_combo full_x2xtreme-eng
+add_lunch_combo cm_x2xtreme-user
+add_lunch_combo cm_x2xtreme-userdebug
+add_lunch_combo cm_x2xtreme-eng
